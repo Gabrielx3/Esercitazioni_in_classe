5264ebba8149af7f5db296867741c008379d874c
Gabrielx3 <scottixgabriel@gmail.com>
2026-03-13 20:10:07 +0100

    Add files via upload

---
+++ ese2.java
@@ -0,0 +1,13 @@
+public class ese2 {
+    public static void main(String[] args) {
+
+        int numero = 1;
+        double prezzo = 1.2;
+        String messaggio = "mess";
+
+        System.out.println("Valore intero: " + numero);
+        System.out.println("Valore double: " + prezzo);
+        System.out.println("Valore stringa: " + messaggio);
+    }
+}
+
