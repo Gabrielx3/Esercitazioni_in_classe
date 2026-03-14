c9f1457d49e9444556ce044d25b570f4fe7455b6
Gabrielx3 <scottixgabriel@gmail.com>
2026-03-14 13:03:21 +0100

    modificato

--- ese5.java
+++ ese5.java
@@ -1,4 +1,16 @@
-public class prova{
+public class ese5{
+    private String nome;
+    private int eta;
+
+    public ese5(String nome,int eta){
+        this.nome=nome;
+        this.eta=eta;
+    }
+public void presentati(){
+    System.out.println(this.nome+this.eta);
+}
+public static void main(String[]args){
 
 
+}
 }
