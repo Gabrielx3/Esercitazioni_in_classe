677fe9adb16ce2bc375de0d5b695ac0836b227d2
Gabrielx3 <scottixgabriel@gmail.com>
2026-03-14 12:54:46 +0100

    modificato

--- prova.java
+++ prova.java
@@ -1,27 +1,4 @@
-public class prova {
-    
-    class Persona{
-    String nome;
-    int eta;
+public class prova{
 
-    void presentati() {
-        System.out.println("Ciao, mi chiamo " + nome + " e ho " + eta + " anni");
-    }
+
 }
-
-    public static void main(String[] args) {
-
-        Persona p1 = new Persona();
-        p1.nome = "Marco";
-        p1.eta = 30;
-
-        Persona p2 = new Persona();
-        p2.nome = "Luca";
-        p2.eta = 25;
-
-        p1.presentati();
-        p2.presentati();
-    }
-}
-
-
